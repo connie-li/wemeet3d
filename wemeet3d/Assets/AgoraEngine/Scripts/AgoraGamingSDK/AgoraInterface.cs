708721f2994695210685aea94307dbcb1dd61f47
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2020-11-18 00:05:45 -0600

    setting up rtc engine

---
+++ wemeet3d/Assets/AgoraEngine/Scripts/AgoraGamingSDK/AgoraInterface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using agora_gaming_rtc;
+
+public class AgoraInterface : MonoBehaviour
+{
+    private static string appId = "5b605d324a8d4fe082de1236e69872af";
+    public IRtcEngine mRtcEngine;
+
+    //initializating agora RTC engine
+    public void loadEngine()
+    {
+      //starting SDK
+      Debug.Log("initializating engine");
+
+      if(mRtcEngine != null)
+      {
+        Debug.Log("Engine already exists");
+        return;
+      }
+      //initializating RTC engine with appID
+      mRtcEngine = IRtcEngine.getEngine(appId);
+
+    }
+}
