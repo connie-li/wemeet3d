c22f48342813c38d99f22d9e305d41899493a926
Connie <constanceli@ku.edu>
2021-02-04 01:07:18 -0600

    feat (UI): add password show/hide functionality; UI improvements
    - Join Room Menu: replace "show password" button with toggle
    - Create Room Menu: add "show password" toggle
    - add password toggling scripts, adapted from https://gamedev.stackexchange.com/a/133065
    - UI design tweaks

---
+++ Assets/UIController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public UnityEngine.UI.InputField pwInputJoin = null;
+    public UnityEngine.UI.InputField pwInputCreate = null;
+
+    public void ToggleInputTypeJoinPW()
+    {
+        if (this.pwInputJoin != null)
+        {
+            if (this.pwInputJoin.contentType == InputField.ContentType.Password)
+            {
+                this.pwInputJoin.contentType = InputField.ContentType.Standard;
+            }
+            else
+            {
+                this.pwInputJoin.contentType = InputField.ContentType.Password;
+            }
+
+            this.pwInputJoin.ForceLabelUpdate();
+        }
+    }
+
+    public void ToggleInputTypeCreatePW()
+    {
+        if (this.pwInputCreate != null)
+        {
+            if (this.pwInputCreate.contentType == InputField.ContentType.Password)
+            {
+                this.pwInputCreate.contentType = InputField.ContentType.Standard;
+            }
+            else
+            {
+                this.pwInputCreate.contentType = InputField.ContentType.Password;
+            }
+
+            this.pwInputCreate.ForceLabelUpdate();
+        }
+    }
+}
