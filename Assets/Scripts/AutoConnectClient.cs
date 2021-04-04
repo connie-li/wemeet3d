bd77b8aa84e4ddb2b71f7d8d8904f9fb0117e5d7
Victoria <vica.chevchenco@gmail.com>
2021-04-04 17:06:57 -0500

    Added offline scene for server connection

---
+++ Assets/Scripts/AutoConnectClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class AutoConnectClient : MonoBehaviour
+{
+
+  [SerializeField] NetworkManager networkManager;
+
+  public void Start()
+  {
+    if (!Application.isBatchMode)
+    {
+      Debug.Log ("Client Build");
+      networkManager.StartClient();
+    }else{
+      Debug.Log ("Server Build");
+    }
+  }
+
+    public void JoinLocal ()
+    {
+      networkManager.networkAddress = "localhost";
+      networkManager.StartClient();
+
+    }
+}
